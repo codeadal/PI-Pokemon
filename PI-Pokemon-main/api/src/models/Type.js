a6f95dc032a9e54929d9a36db2e611a0d9074e59
codeadal <orta.adal@gmail.com>
2023-05-02 11:23:14 -0600

    Modelo Type Creado

---
+++ PI-Pokemon-main/api/src/models/Type.js
@@ -0,0 +1,18 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('type', {
+    // este podríamos omitirlo porque en automatico se generaría el Id, sin embargo para efectos practicos del PI lo he declarado
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+  });
+};
